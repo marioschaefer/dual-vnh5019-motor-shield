cacf3991cdbd303e9e2a3c186ee4d2d2c0ea185a
marioschaefer <m.schaefer@unitedro.de>
2018-06-01 22:32:01 +0200

    removed all references for the second motor and renamed to SingleVNH5019MotorShield

--- examples/Demo/Demo.ino
+++ examples/Demo/Demo.ino
@@ -1,6 +1,6 @@
-#include "DualVNH5019MotorShield.h"
+#include "SingleVNH5019MotorShield.h"
 
-DualVNH5019MotorShield md;
+SingleVNH5019MotorShield md;
 
 void stopIfFault()
 {
@@ -9,17 +9,12 @@ void stopIfFault()
     Serial.println("M1 fault");
     while(1);
   }
-  if (md.getM2Fault())
-  {
-    Serial.println("M2 fault");
-    while(1);
-  }
 }
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Dual VNH5019 Motor Shield");
+  Serial.println("Single VNH5019 Motor Shield");
   md.init();
 }
 
@@ -57,42 +52,6 @@ void loop()
     {
       Serial.print("M1 current: ");
       Serial.println(md.getM1CurrentMilliamps());
-    }
-    delay(2);
-  }
-
-  for (int i = 0; i <= 400; i++)
-  {
-    md.setM2Speed(i);
-    stopIfFault();
-    if (i%200 == 100)
-    {
-      Serial.print("M2 current: ");
-      Serial.println(md.getM2CurrentMilliamps());
-    }
-    delay(2);
-  }
-  
-  for (int i = 400; i >= -400; i--)
-  {
-    md.setM2Speed(i);
-    stopIfFault();
-    if (i%200 == 100)
-    {
-      Serial.print("M2 current: ");
-      Serial.println(md.getM2CurrentMilliamps());
-    }
-    delay(2);
-  }
-  
-  for (int i = -400; i <= 0; i++)
-  {
-    md.setM2Speed(i);
-    stopIfFault();
-    if (i%200 == 100)
-    {
-      Serial.print("M2 current: ");
-      Serial.println(md.getM2CurrentMilliamps());
     }
     delay(2);
   }
